1eccada8b05b1f0ec83c709269aedfc76bef0141
Jack Nunnink <jack.nunnink@protonmail.com>
2022-06-09 00:17:53 -0400

    finished

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const { logger, validateUserId, validateUser, validatePost } = require('./middleware/middleware');
 const server = express();
+const usersRouter = require('./users/users-router');
 
-// remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+server.use(logger);
+server.use('/api/users', usersRouter);
+
+// const postsRouter = require('./posts/posts-model');
 
 // global middlewares and the user's router need to be connected here
+// server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
